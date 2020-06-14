5e99a04e412979fa108393e3b8b531afef72933b
Scarnouse <manquesada@gmail.com>
2020-06-14 18:27:35 +0200

    e2e and circleci

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
   production: true,
+  appId: '32b479b7daab932ef8acad5beabeae3e',
+  baseUrl: 'http://',
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   appId: '32b479b7daab932ef8acad5beabeae3e',
   baseUrl: 'http://',
 }
